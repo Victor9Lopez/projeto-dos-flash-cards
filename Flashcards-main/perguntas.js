be41e6afacb58bf35a3725cd345e6cb9749d570f
Victor.lopez <140832322+Victor9Lopez@users.noreply.github.com>
2024-09-20 08:14:34 -0300

    Update perguntas.js

--- Flashcards-main/perguntas.js
+++ Flashcards-main/perguntas.js
@@ -137,7 +137,269 @@ criaCartao(
 )
 
 criaCartao(
-    '',
-    '',
-    ''
+    'Geografia',
+    'Qual a nacionalidade de Che Guevara',
+    'Argentina'
+)
+
+criaCartao(
+    'Biologia',
+    'Quais são os três predadores do reino animal reconhecidos pela habilidade de caçar em grupo, se camuflar para surpreender as presas e possuir sentidos apurados, respectivamente:',
+    'Hiena, urso branco e lobo cinzento.'
+)
+
+criaCartao(
+    'Fisíca',
+    'Em que ordem surgiram os modelos atômicos?',
+    'Dalton, Thomson, Rutherford, Rutherford-Bhor.'
+)
+
+criaCartao(
+    'Hitória',
+    'Em que período  da pré-historia o fogo foi decoberto?',
+    'Paleolítico'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a montanha mais alta do Brasil?',
+    'Pico da Neblina'
+)
+
+criaCartao(
+    'Fisíca',
+    'Qual a velocidade da luz?',
+    '299 792 458 metros por segundo(m/s)'
+)
+
+criaCartao(
+    'Geografia',
+    'Em qual local da Ásia o português é língua oficial?',
+    'Macau'
+)
+
+criaCartao(
+    'Ingles',
+    '"It is seix twenty" ou "twenty past six".Que horas são em ingles?',
+    '6:20'
+)
+
+criaCartao(
+    'português',
+    'Como é a conjugação do verbo caber na 1° pessoa do singular do presente do indicativo?',
+    'Eu caibo'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais construções famosas ficam nos Estados Unidos?',
+    'Etátua da liberdade, Golden Gate Bridge e Empire State Building'
+)
+
+criaCartao(
+    'Biologia',
+    'Quais doenças são sexualmente transmissiveis?',
+    'Gonorreio, clamidia e sifilis'
+)criaCartao(
+    'Astronomia',
+    'O que é gravidade?',
+    'De acordo com a relatividade geral, é a deformação do espaço tempo causado por um objeto massivo como uma estrela ou um planeta, por exemplo'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é câncer?',
+    'É o crescimento desordenado das células devido a alterações no mecanismo celular'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Austrália?',
+    'Camberra'
+)
+
+criaCartao(
+    'Inglês',
+    'Como se diz mar em inglês?',
+    'Sea'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a raiz quadrada de -4?',
+    'É mais ou menos 2i'
+)
+
+criaCartao(
+    'Química',
+    'O que significa densidade?',
+    'É quanto determinada massa de um elemento ocupa certo espaço'
+)
+
+criaCartao(
+    'Física',
+    'Por qual descoberta Albert Einstein ganhou o prêmio nobel de física?',
+    'Efeito fotoelétrico'
+)
+
+criaCartao(
+    'Biologia',
+    'Normalmente, quantos litros de sangue uma pessoa tem?Em média, quantos são retirados numa doação de sangue?',
+    'Entre 4 a 6 litros. são retirados 450 mililitros'
+)
+
+criaCartao(
+    'Filosofia',
+    'De quem é a famosa frase "Penso, logo existo"?',
+    'Descartes'
+)
+
+criaCartao(
+    'História',
+    'De onde é a invenção do chuveiro elétrico?',
+    'Brasil'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais o menor e maior país do mundo?',
+    'Vaticano e Rússia'
+)
+
+criaCartao(
+    'História',
+    'Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
+    'João Goulart'
+)
+
+criaCartao(
+    'Português',
+    'Qual o grupo em que todas as palavras foram escritas corretamente?',
+    'Asteriscos, beneficente, meteorologia, entretido.'
+)
+
+criaCartao(
+    'História',
+    'Qual o livro mais vendido no mundo a seguir à Bíblia?',
+    'Dom Quixote'
+)
+
+criaCartao(
+    'Matematica',
+    'Quantas casa decimais tem o número pi?',
+    'Infinitas'
+)
+
+criaCartao(
+    'Quimíca',
+    'Atualmente, quantos elementos quimicos a tabela periodica possui?',
+    '118'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais o países que tem a maior e a menor expectativa de vida do mundo?',
+    'Japão e Serra Leoa'
+)
+
+criaCartao(
+    'Ingles',
+    'O que a palavra legend significa em português?',
+    'Lenda'
+)
+
+criaCartao(
+    'Educação Fisíca',
+    'Qual o número de jogadores em cada time numa partida de futebol?',
+    '7'
+)
+
+criaCartao(
+    'História',
+    'Quais a duas datas qe são comemoradas em novembro?',
+    'Proclamação da Republica e Dia Nacional da Consciência Negra'
+)
+
+criaCartao(
+    'Artes',
+    'Quem pintou "Guernica"?',
+    'Pablo Picasso'
+)
+
+criaCartao(
+    'Fisíca',
+    'Quanto tempo a luz do Sol demora para chegar à Terra?',
+    '8 minutos'
+)
+
+criaCartao(
+    'Espanhol',
+    'Qual a tradução da frase "Fabiano cogió su saco antes de salir',
+    'Fabiano pegou o saco antes de sair?'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a nacionalidade de Che Guevara',
+    'Argentina'
+)
+
+criaCartao(
+    'Biologia',
+    'Quais são os três predadores do reino animal reconhecidos pela habilidade de caçar em grupo, se camuflar para surpreender as presas e possuir sentidos apurados, respectivamente:',
+    'Hiena, urso branco e lobo cinzento.'
+)
+
+criaCartao(
+    'Fisíca',
+    'Em que ordem surgiram os modelos atômicos?',
+    'Dalton, Thomson, Rutherford, Rutherford-Bhor.'
+)
+
+criaCartao(
+    'Hitória',
+    'Em que período  da pré-historia o fogo foi decoberto?',
+    'Paleolítico'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a montanha mais alta do Brasil?',
+    'Pico da Neblina'
+)
+
+criaCartao(
+    'Fisíca',
+    'Qual a velocidade da luz?',
+    '299 792 458 metros por segundo(m/s)'
+)
+
+criaCartao(
+    'Geografia',
+    'Em qual local da Ásia o português é língua oficial?',
+    'Macau'
+)
+
+criaCartao(
+    'Ingles',
+    '"It is seix twenty" ou "twenty past six".Que horas são em ingles?',
+    '6:20'
+)
+
+criaCartao(
+    'português',
+    'Como é a conjugação do verbo caber na 1° pessoa do singular do presente do indicativo?',
+    'Eu caibo'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais construções famosas ficam nos Estados Unidos?',
+    'Etátua da liberdade, Golden Gate Bridge e Empire State Building'
+)
+
+criaCartao(
+    'Biologia',
+    'Quais doenças são sexualmente transmissiveis?',
+    'Gonorreio, clamidia e sifilis'
 )
